f00b61b6355445b66be3c9621f8f73da1f452313
Teguh Arzaqi <teguhdarmawanarzaqi@gmail.com>
2026-07-30 10:44:49 +0700

    Fix logout authentication

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -60,4 +60,15 @@ public function login(Request $request)
 
     return redirect()->route('dashboard');
 }
+
+public function logout(Request $request)
+{
+    Auth::logout();
+
+    $request->session()->invalidate();
+
+    $request->session()->regenerateToken();
+
+    return redirect()->route('login');
+}
 }
